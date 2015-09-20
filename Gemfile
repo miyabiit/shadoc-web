6ca1388fce4a0aaeed0de6bfb02e63ac5b3f6d6f
baker@BALLOONGA <yamada.masahiro@gmail.com>
2015-09-20 16:48:29 +0900

    SHADOC web interface by sinatra

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+gem "activerecord"
+gem 'mysql2', '~> 0.3.18'
+gem 'sinatra'
---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'active_record'
+require 'mysql2'
+require 'sinatra'
+
+ActiveRecord::Base.configurations = YAML.load_file('database.yml')
+ActiveRecord::Base.establish_connection(:development)
+
+class Location < ActiveRecord::Base
+end
+
+get '/' do
+	@locations = Location.all.order('created_at asc').limit(30)
+	erb :index
+end
+
+post '/' do
+	key = @params[:search]	
+	if key 
+		@locations = Location.where('keywords like ?', "%#{key}%")
+	else
+		@locations = Location.all.order('created_at asc').limit(30)
+	end
+	erb :index
+end
+
